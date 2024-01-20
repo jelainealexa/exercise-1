261737e0aa621a069f78db3f3dac9763b9b60af0
jelainealexa <jelaine.alexa0903@gmail.com>
2024-01-20 18:25:11 +0800

    Return product if less than 1000

--- exercise-1.py
+++ exercise-1.py
@@ -4,3 +4,5 @@ def calculate(number1, number2):
     # Calculate the product of two numbers
     product = number1 * number2
     # Check if the product is less than 1000
+    if product < 1000:
+        return product
